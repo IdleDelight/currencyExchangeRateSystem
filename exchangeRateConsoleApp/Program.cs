fcb9f559015a5188deb57b9a70ad125618a5c08a
KristianLF <123668111+IdleDelight@users.noreply.github.com>
2023-08-13 19:21:46 +0200

    ...missed a part of previous - un-saved file

--- exchangeRateConsoleApp/Program.cs
+++ exchangeRateConsoleApp/Program.cs
@@ -1,4 +1,7 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Net.Http;
 using Newtonsoft.Json.Linq;
 
 namespace ExchangeRateConsoleApp
@@ -16,67 +19,67 @@ namespace ExchangeRateConsoleApp
             rateCache["latest"] = latestRates;
 
             while (true) {
-                string fromCurrency;
+                string fromCurrency, toCurrency, dateInput;
+                decimal amount;
+                DateTime parsedDate;
+
+                Console.Clear();
+                PrintHeader("The Currency Converter");
                 do {
-                    Console.WriteLine("####################################");
-                    Console.WriteLine("#### The Currency Converter App ####");
-                    Console.WriteLine("####################################");
                     Console.Write("Enter the source currency code: ");
                     fromCurrency = Console.ReadLine()!.ToUpper(CultureInfo.InvariantCulture);
                     if (!IsValidCurrency(fromCurrency)) {
-                        Console.Write($"Invalid currency. Please enter a valid currency codes: ");
+                        Console.WriteLine($"Invalid currency. Please enter a valid currency code.");
                     }
                 } while (!IsValidCurrency(fromCurrency));
 
-                decimal amount;
+                Console.Clear();
+                PrintHeader("The Currency Converter");
+                PrintSubHeader($"{fromCurrency}");
                 do {
                     Console.Write($"Enter the {fromCurrency} amount: ");
-                    var input = Console.ReadLine();
-                    if (!decimal.TryParse(input, out amount) || amount <= 0) {
-                        Console.WriteLine("Invalid amount. Please enter a valid number.");
-                    }
-                } while (amount <= 0);
+                    if (decimal.TryParse(Console.ReadLine(), out amount) && amount > 0)
+                        break;
+                    Console.WriteLine("Invalid amount. Please enter a valid number.");
+                } while (true);
 
-                string toCurrency;
+                Console.Clear();
+                PrintHeader("The Currency Converter");
+                PrintSubHeader($"{fromCurrency} {amount} to");
                 do {
                     Console.Write("Enter the target currency code: ");
                     toCurrency = Console.ReadLine()!.ToUpper(CultureInfo.InvariantCulture);
                     if (!IsValidCurrency(toCurrency)) {
-                        Console.Write($"\nInvalid currency. Please enter a valid currency codes: ");
+                        Console.WriteLine($"Invalid currency. Please enter a valid currency code.");
                     }
                 } while (!IsValidCurrency(toCurrency));
 
-                string dateInput;
-                DateTime parsedDate;
+                Console.Clear();
+                PrintHeader("The Currency Converter");
+                PrintSubHeader($"{fromCurrency} {amount} to {toCurrency}");
                 do {
                     Console.WriteLine("Choose exchange rates from a specific date (YYYY-MM-DD),");
                     Console.Write("or press [Enter] for latest rates: ");
                     dateInput = Console.ReadLine()!;
-
                     if (string.IsNullOrWhiteSpace(dateInput)) {
                         dateInput = "latest";
                         break;
                     }
+                } while (!DateTime.TryParseExact(dateInput, "yyyy-MM-dd", null, DateTimeStyles.None, out parsedDate));
 
-                    if (!DateTime.TryParseExact(dateInput, "yyyy-MM-dd", null, DateTimeStyles.None, out parsedDate)) {
-                        Console.WriteLine("\nInvalid date format. Please enter a date in the format");
-                        Console.Write("YYYY-MM-DD: ");
-                    }
-
-                } while (!DateTime.TryParseExact(dateInput, "yyyy-MM-dd", null, DateTimeStyles.None, out parsedDate) && !string.IsNullOrWhiteSpace(dateInput));
 
                 if (!rateCache.ContainsKey(dateInput)) {
                     var rates = FetchExchangeRates(dateInput);
                     rateCache[dateInput] = rates;
                 }
 
+                Console.Clear();
+                PrintHeader("The Currency Converter");
+                PrintSubHeader($"Using {dateInput} rates for {fromCurrency} and {toCurrency}");
                 decimal result = ConvertCurrency(fromCurrency, toCurrency, amount, rateCache[dateInput]);
-                Console.WriteLine("####################################");
-                Console.WriteLine($"\n{fromCurrency} to {toCurrency}, with {dateInput} rates");
                 Console.WriteLine($"{fromCurrency} {amount} is approximately {toCurrency} {result:N2}");
-                Console.WriteLine("####################################");
+                PrintFooter("Press [Q] to quit, or any other key to continue...");
 
-                Console.WriteLine("\n\nPress [Q] to quit, or any other key to continue...");
                 if (Console.ReadLine()!.ToLower() == "q") break;
             }
         }
@@ -111,6 +114,26 @@ namespace ExchangeRateConsoleApp
 
             decimal toEuroRate = 1 / rates[from].Value<decimal>();
             return amount * toEuroRate * rates[to].Value<decimal>();
+        }
+
+        private static void PrintHeader( string text )
+        {
+            Console.WriteLine("===================================");
+            Console.WriteLine($"   {text}");
+            Console.WriteLine("===================================");
+        }
+
+        private static void PrintSubHeader( string text )
+        {
+            Console.WriteLine(text);
+            Console.WriteLine("-----------------------------------");
+        }
+
+        private static void PrintFooter( string text )
+        {
+            Console.WriteLine("===================================");
+            Console.WriteLine(text);
+            Console.WriteLine("===================================");
         }
     }
 }
